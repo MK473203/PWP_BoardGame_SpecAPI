0e0e03d9d3c5d48fa01c8daf0dd338fe15ebc8c2
Markus <mk473203@outlook.com>
2024-04-24 18:50:38 +0300

    Basic functionality working

--- test_client.py
+++ test_client.py
@@ -1,21 +1,51 @@
 import requests
+import json
 import pika
 
-GAMES_URL = "http://localhost:5000/api/games/"
+API_URL = "http://localhost:5000"
 
 
-if __name__ == "__main__":
-	resp = requests.get(GAMES_URL)
+if __name__ == "__main__":  # pragma: no cover
 
-	game = resp.json()["items"][0]
+    def notification_handler(ch, method, properties, body):
+        print(body)
+        
+    resp = requests.get(API_URL + "/api/games/")
 
-	print("trying to spectate game " + game["id"])
+    game = resp.json()["items"][0]
 
-	resp = requests.get(game["@controls"]["self"]["href"])
+    print("trying to spectate game " + game["id"])
 
-	game = resp.json()
-	print(game["@controls"]["boardgame:spectate"]["href"])
+    resp = requests.get(API_URL + game["@controls"]["self"]["href"])
 
-	resp = requests.get(game["@controls"]["boardgame:spectate"]["href"])
+    game = resp.json()
+    print(game["@controls"]["boardgame:spectate"]["href"])
 
-	
+    resp = requests.get(game["@controls"]["boardgame:spectate"]["href"])
+
+    RABBITMQ_EXCHANGE = resp.json()["exchange"]
+    RABBITMQ_BROKER_URL = resp.json()["@controls"]["amqp-url"]
+
+    print(RABBITMQ_EXCHANGE)
+    print(RABBITMQ_BROKER_URL)
+
+    print("create connection")
+    connection = pika.BlockingConnection(
+        pika.URLParameters(RABBITMQ_BROKER_URL))
+    channel = connection.channel()
+    channel.exchange_declare(
+        exchange=RABBITMQ_EXCHANGE,
+        exchange_type="fanout"
+    )
+    result = channel.queue_declare(queue="", exclusive=True, auto_delete=True)
+    channel.queue_bind(
+        exchange=RABBITMQ_EXCHANGE,
+        queue=result.method.queue
+    )
+    channel.basic_consume(
+        queue=result.method.queue,
+        on_message_callback=notification_handler,
+        auto_ack=True
+    )
+    print("start consuming")
+    channel.start_consuming()
